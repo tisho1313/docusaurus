5fc54861bfed4af9c070101cd5d5c0d0343a8e0a
tisho1313 <72949336+tisho1313@users.noreply.github.com>
2025-02-25 22:16:48 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://white-cliff-0fd694903.4.azurestaticapps.net',
+  url: 'https://proud-cliff-085c9d203.4.azurestaticapps.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
