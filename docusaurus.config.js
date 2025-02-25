b69726a9f2a03a550e0525d020d8b3292258dda1
tisho1313 <72949336+tisho1313@users.noreply.github.com>
2025-02-25 09:16:06 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://gray-sea-05a08e703.4.azurestaticapps.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
